ebeec4f9136c4fc8c679b92442c0391fd4c68a13
Joacil Amarante <joacil.amarante@gmail.com>
2022-11-10 11:45:01 -0400

    .

---
+++ src/main/java/alura/com/servelet/gerenciador/olamundo.java
@@ -0,0 +1,30 @@
+package alura.com.servelet.gerenciador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class olamundo extends HttpServlet {
+	
+	
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("olá mundo 2");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		
+	}
+	
+
+}
